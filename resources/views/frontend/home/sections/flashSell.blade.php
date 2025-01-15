dd22546e2dfba3bc69f20b3a8e4d7297662ad1db
AbdulrahmanMaktabi <abdurahmanmaktabi@gmail.com>
2025-01-15 09:27:26 +0300

    Frontend Adding add to cart feature in product cart

--- resources/views/frontend/home/sections/flashSell.blade.php
+++ resources/views/frontend/home/sections/flashSell.blade.php
@@ -18,56 +18,84 @@
                 @foreach ($flashSale->items as $item)
                     <div class="row flash_sell_slider">
                         <div class="col-xl-3 col-sm-6 col-lg-4">
-                            <div class="wsus__product_item">
-                                <span
-                                    class="wsus__new">{{ ($item->product->is_best ? 'Best' : $item->product->is_top) ? 'Top' : 'New' }}</span>
-                                <span class="wsus__minus">
-                                    {{ discountPercentage($item->product->price, $item->discounted_price) }}%
-                                </span>
-                                <a class="wsus__pro_link"
-                                    href="{{ route('frontend.product', ['slug' => $item->product->slug]) }}">
-                                    <img src="{{ asset($item->product->thumb_image) }}" alt="product"
-                                        class="img-fluid w-100 img_1" />
-                                    @if ($item->product->imageGallery[0])
-                                        <img src="{{ asset($item->product->imageGallery[0]->image) }}" alt="product"
-                                            class="img-fluid w-100 img_2" />
-                                    @endif
-                                </a>
-                                <ul class="wsus__single_pro_icon">
-                                    <li><a href="#" data-bs-toggle="modal"
-                                            data-bs-target="#exampleModal-{{ $item->id }}"><i
-                                                class="far fa-eye"></i></a></li>
-                                    <li><a href="#"><i class="far fa-heart"></i></a></li>
-                                    <li><a href="#"><i class="far fa-random"></i></a>
-                                </ul>
-                                <div class="wsus__product_details">
-                                    <a class="wsus__category"
-                                        href="{{ route('frontend.product', ['slug' => $item->product->slug]) }}">{{ $item->product->category->name }}
+                            <form class="shopping-cart-form" id="shopping-cart-form-{{ $item->product->id }}"
+                                data-product="{{ $item->product->slug }}">
+                                <input type="hidden" name="productID" value="{{ $item->product->id }}">
+                                <input type="hidden" name="qty" value="1">
+                                <div class="wsus__product_item">
+                                    <span
+                                        class="wsus__new">{{ ($item->product->is_best ? 'Best' : $item->product->is_top) ? 'Top' : 'New' }}</span>
+                                    <span class="wsus__minus">
+                                        {{ discountPercentage($item->product->price, $item->discounted_price) }}%
+                                    </span>
+                                    <a class="wsus__pro_link"
+                                        href="{{ route('frontend.product', ['slug' => $item->product->slug]) }}">
+                                        <img src="{{ asset($item->product->thumb_image) }}" alt="product"
+                                            class="img-fluid w-100 img_1" />
+                                        @if ($item->product->imageGallery[0])
+                                            <img src="{{ asset($item->product->imageGallery[0]->image) }}"
+                                                alt="product" class="img-fluid w-100 img_2" />
+                                        @endif
                                     </a>
-                                    <p class="wsus__pro_rating">
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star"></i>
-                                        <i class="fas fa-star-half-alt"></i>
-                                        <span>(133 review)</span>
-                                    </p>
-                                    <a class="wsus__pro_name"
-                                        href="{{ route('frontend.product', ['slug' => $item->product->slug]) }}">{{ $item->product->name }}</a>
-                                    @if (checkDiscount($item?->product))
-                                        <p class="wsus__price">
-                                            {{ $generalSettings->currency_icon }}{{ $item->product->offer_price - $item->discounted_price }}
-                                            <del>${{ $item->product->price }}</del>
+                                    <ul class="wsus__single_pro_icon">
+                                        <li><a href="#" data-bs-toggle="modal"
+                                                data-bs-target="#exampleModal-{{ $item->id }}"><i
+                                                    class="far fa-eye"></i></a></li>
+                                        <li><a href="#"><i class="far fa-heart"></i></a></li>
+                                        <li><a href="#"><i class="far fa-random"></i></a>
+                                    </ul>
+                                    <div class="wsus__product_details">
+                                        <a class="wsus__category"
+                                            href="{{ route('frontend.product', ['slug' => $item->product->slug]) }}">{{ $item->product->category->name }}
+                                        </a>
+                                        <p class="wsus__pro_rating">
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star"></i>
+                                            <i class="fas fa-star-half-alt"></i>
+                                            <span>(133 review)</span>
                                         </p>
-                                    @else
-                                        <p class="wsus__price">
-                                            {{ $generalSettings->currency_icon }}{{ $item->product->price - $item->discounted_price }}
-                                            <del>${{ $item->product->price }}</del>
-                                        </p>
-                                    @endif
-                                    <a class="add_cart" href="#">add to cart</a>
+                                        <a class="wsus__pro_name"
+                                            href="{{ route('frontend.product', ['slug' => $item->product->slug]) }}">{{ $item->product->name }}</a>
+                                        @if (checkDiscount($item?->product))
+                                            <p class="wsus__price">
+                                                {{ $generalSettings->currency_icon }}{{ $item->product->offer_price - $item->discounted_price }}
+                                                <del>${{ $item->product->price }}</del>
+                                            </p>
+                                        @else
+                                            <p class="wsus__price">
+                                                {{ $generalSettings->currency_icon }}{{ $item->product->price - $item->discounted_price }}
+                                                <del>${{ $item->product->price }}</del>
+                                            </p>
+                                        @endif
+                                        @if (count($item->product->variants) > 0)
+                                            <div class="wsus__selectbox">
+                                                <div class="row">
+                                                    @foreach ($item->product->variants as $variant)
+                                                        <div class="col-xl-6 col-sm-6">
+                                                            <h5 class="mb-2">{{ $variant->name }}:</h5>
+                                                            <select class="select_2" name="variants[]"
+                                                                data-variant-price="{{ $variant->price }}"
+                                                                data-product-id="{{ $item->product->id }}">
+                                                                {{-- <option disabled>default select</option> --}}
+                                                                @foreach ($variant->variantItems as $option)
+                                                                    <option value="{{ $option->id }}"
+                                                                        data-price="{{ $option->price }}">
+                                                                        {{ $option->name }} +(${{ $option->price }})
+                                                                    </option>
+                                                                @endforeach
+                                                            </select>
+                                                        </div>
+                                                    @endforeach
+                                                </div>
+                                            </div>
+                                        @endif
+                                        <input type="submit" value="ADD TO CART" class="add_cart">
+                                        {{-- <a class="add_cart" href="#">add to cart</a> --}}
+                                    </div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
                     </div>
                 @endforeach
@@ -76,6 +104,30 @@
     </section>
 
     @push('scripts')
+        <script>
+            // Get the CSRF token value
+            let csrfToken = $('meta[name="csrf-token"]').attr('content');
+            $('.shopping-cart-form').on('submit', function(e) {
+                e.preventDefault();
+                let formId = $(this).attr('id');
+                let formData = $(this).serialize();
+                console.log('formData = ' + formData);
+                let productID = $('')
+                $.ajax({
+                    method: 'POST',
+                    url: "{{ route('frontend.cart.add') }}",
+                    data: formData + '&_token=' + csrfToken,
+                    success: function(response) {
+                        console.log(`Form ${formId} Response:`, response);
+                        alert('Item added to cart successfully!');
+                    },
+                    error: function(error) {
+                        console.error(`Form ${formId} Error:`, error);
+                        alert('An error occurred. Please try again.');
+                    }
+                });
+            });
+        </script>
         <script>
             // Get the Flash Sale End Time from Blade Variable
             var endTime = new Date('{{ $flashSale->end_time }}');
